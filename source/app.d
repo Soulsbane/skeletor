2de886ea713db780e1aec120b3827043b7503f69
Paul Crane <paul@acheronsoft.com>
2016-05-16 01:48:09 -0500

    Remove config initialization from app.d and add string imports path to dub.sdl.

--- source/app.d
+++ source/app.d
@@ -1,12 +1,6 @@
 import std.stdio;
 import raijin;
 
-AppConfig Config_;
-
-static this()
-{
-auto AppConfig_ = AppConfig("Raijinsoft", "skeletor", defaultConfigFileData);
-}
 void main(string[] arguments)
 {
 }
