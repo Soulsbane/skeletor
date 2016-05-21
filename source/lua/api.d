5c354a5779c18a6b19e157d57ac70285b8873e28
Paul Crane <paul@acheronsoft.com>
2016-05-21 00:38:01 -0500

    Add path API functions and raijinlib generator.

--- source/lua/api.d
+++ source/lua/api.d
@@ -12,10 +12,20 @@ static this()
 	immutable string defaultConfigFileData = import("default-app.config");
 	_Config = AppConfig("Raijinsoft", "skeletor", defaultConfigFileData);
 
-	ensurePathExists(getTemplatesPath());
+	ensurePathExists(getGeneratorPath());
 }
 
-string getTemplatesPath(const string language = string.init)
+string getGeneratorPath()
 {
-	return _Config.path.getConfigDir("templates", language);
+	return _Config.path.getConfigDir("generators");
+}
+
+string getGeneratorLanguagePath(const string language = string.init)
+{
+	return _Config.path.getConfigDir("generators", language);
+}
+
+string getGeneratorPathFor(const string language = string.init, const string generatorName = string.init)
+{
+	return _Config.path.getConfigDir("generators", language, generatorName);
 }
