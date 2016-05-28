7e1565293969ec899355637b25e30b25ffab2d3a
Paul Crane <paul@acheronsoft.com>
2016-05-28 01:22:06 -0500

    Check for valid args before running generation routines.

--- source/app.d
+++ source/app.d
@@ -6,20 +6,39 @@ import inputcollector;
 import lua.generator;
 import lua.api.path;
 
+class Arguments : CommandLineArgs
+{
+	override void onValidArgs()
+	{
+		validArgsPassed_ = true;
+	}
+
+	bool hasValidArgs() @property const
+	{
+		return validArgsPassed_;
+	}
+
+private:
+	bool validArgsPassed_;
+}
+
 void main(string[] arguments)
 {
-	auto args = new CommandLineArgs;
+	auto args = new Arguments;
 
 	args.addCommand("language", "d", "The name of language to generate a project for.");
 	args.addCommand("generator", "raijin", "The name of the generator to use.");
 	args.process(arguments);
 
-	CollectedValues values = collectValues();
-	LuaGenerator generator = LuaGenerator("d", "raijin");
-	immutable bool succeeded = generator.create(values);
-
-	if(!succeeded)
+	if(args.hasValidArgs)
 	{
-		writeln("Generator not found!");
+		CollectedValues values = collectValues();
+		LuaGenerator generator = LuaGenerator("d", "raijin");
+		immutable bool succeeded = generator.create(values);
+
+		if(!succeeded)
+		{
+			writeln("Generator not found!");
+		}
 	}
 }
