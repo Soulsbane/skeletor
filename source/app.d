04f8e022131c2f5e6878d16baa8e3227e30203cd
Paul Crane <paul@acheronsoft.com>
2016-05-28 01:11:03 -0500

    Add initial command line argument support.

--- source/app.d
+++ source/app.d
@@ -8,6 +8,12 @@ import lua.api.path;
 
 void main(string[] arguments)
 {
+	auto args = new CommandLineArgs;
+
+	args.addCommand("language", "d", "The name of language to generate a project for.");
+	args.addCommand("generator", "raijin", "The name of the generator to use.");
+	args.process(arguments);
+
 	CollectedValues values = collectValues();
 	LuaGenerator generator = LuaGenerator("d", "raijin");
 	immutable bool succeeded = generator.create(values);
