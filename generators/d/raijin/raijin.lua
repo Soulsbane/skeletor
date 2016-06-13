1a7eb55fa7a1913b941de8884f7d9cd765b66c55
Paul Crane <paul@acheronsoft.com>
2016-06-12 19:49:03 -0500

    Add first working generator for d.raijin.

--- generators/d/raijin/raijin.lua
+++ generators/d/raijin/raijin.lua
@@ -3,6 +3,13 @@ function OnCreate()
 end
 
 function OnProcessInput(values)
+	Path.CreateDirInOutputDir(ProjectName, "source")
+
+	local dubData = Helpers.ParseTemplate("raijin-dub.sdl")
+	IO.CreateOutputFile(ProjectName .. "/dub.sdl", dubData)
+
+	local appData = Helpers.ParseTemplate("raijin-app.d")
+	IO.CreateOutputFile(ProjectName .. "/source/app.d", appData)
 end
 
 function OnDestroy()
---
+++ generators/d/raijin/templates/raijin-app.d
@@ -0,0 +1,8 @@
+import std.stdio;
+
+import raijin;
+
+void main(string[] arguments)
+{
+	writeln("Raijin imported!!");
+}
