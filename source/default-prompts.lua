6a17405a366c75e5d9466cd5d5005d6294b0aa37
Paul Crane <paul@acheronsoft.com>
2016-07-23 16:12:38 -0500

    Make note that input prompt for ProjectName must not be removed.

--- source/default-prompts.lua
+++ source/default-prompts.lua
@@ -1,3 +1,5 @@
 UserInput.Prompt("Author", "Author ", "Paul Crane");
 UserInput.Prompt("Description", "Description", "Project Description");
+
+--NOTE: Removing this will most like result in an error since ProjectName must be defined.
 UserInput.Prompt("ProjectName", "Project Name(Used as directory name as well): ", "foobar")
