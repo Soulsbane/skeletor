e77594b99081ca0ef7c5aa4c6812b4af5cccb69d
Paul Crane <paul@acheronsoft.com>
2016-11-15 17:50:03 -0600

    Add generator for vanilla D application.

---
+++ generators/d/app/app.lua
@@ -0,0 +1,7 @@
+function OnProcessInput(values)
+	Path.CreateDirInOutputDir("source")
+
+	Helpers.ParseAndCreateOutputFile("dub.sdl", "dub.sdl")
+	Helpers.ParseAndCreateOutputFile(Path.Normalize("source", "app.d"), "app.d")
+	Helpers.ParseAndCreateOutputFile(".gitignore", "gitignore")
+end
