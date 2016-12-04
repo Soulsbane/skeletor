4c19aacdf8b41a405fdc924affabb141e6d747e1
Paul Crane <paul@acheronsoft.com>
2016-12-04 02:54:31 -0600

    Add documentation documenting how API_VERSION works.

--- source/lua/api/package.d
+++ source/lua/api/package.d
@@ -1,5 +1,11 @@
 module lua.api;
 
+// The version is broken down as follows Major.Minor.Revsion.
+// The Major will only be changed in the event that an API was removed.
+// The Minor will only be changed in the event that an API was added.
+// The Revision will only be changed in the event that an API was modified in how it's called or how it works
+// internally if it differs.
+// So using 10200 as an example when changed to a string it would be 1.02.00
 private enum API_VERSION = 10200;
 
 size_t getAPIVersion()
