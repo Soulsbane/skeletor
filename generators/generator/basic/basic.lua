5312f47d8e76fb7a4f52a96241607dd5ee66ea91
Paul Crane <paul@acheronsoft.com>
2017-01-15 00:16:07 -0600

    Add basic generator for generating a new generator.

--- generators/generator/basic/basic.lua
+++ generators/generator/basic/basic.lua
@@ -1,10 +1,19 @@
 function OnCreate()
 	DisableProjectDir() --We don't need a project directory created since we are creating a new generator.
-	UserInput.DisablePrompt("ProjectName")
+	UserInput.DisablePrompt("ProjectName") -- Should really be in DisableProjectDir
 end
 
-function OnProcessInput(values)
-	local language = UserInput.Prompt("ProgrammingLanguage", "Name of the programming language?", "d");
-	local name = UserInput.Prompt("GeneratorName", "The name of your generator?", "foobar");
-	Path.CreateDirInGeneratorDir(language, name)
+function OnProcessInput()
+	UserInput.Prompt("ProgrammingLanguage", "Name of the programming language?", "d")
+	UserInput.Prompt("GeneratorName", "The name of your generator?", "foobar")
+end
+
+function OnFinishedInput()
+	local language = UserInput.GetValueFor("ProgrammingLanguage")
+	local name = UserInput.GetValueFor("GeneratorName")
+
+	if UserInput.HasValueFor("ProgrammingLanguage") and UserInput.HasValueFor("GeneratorName") then
+		Helpers.ParseAndCreateGeneratorFile(language, name, name .. ".toc", "template.toc")
+		Helpers.ParseAndCreateGeneratorFile(language, name, name .. ".lua", "template.lua")
+	end
 end
