c349a3f23201a6bf176d73c86a9ca65f5128f50e
Paul Crane <paul@acheronsoft.com>
2017-02-04 21:01:36 -0600

    Rename d.app generators template app.d.

--- generators/d/app/app.lua
+++ generators/d/app/app.lua
@@ -2,6 +2,6 @@ function OnFinishedInput(values)
 	Path.CreateDirInOutputDir("source")
 
 	Helpers.ParseAndCreateOutputFile("dub.sdl", "d.app.dub.sdl")
-	Helpers.ParseAndCreateOutputFile(Path.Normalize("source", "app.d"), "app.d")
+	Helpers.ParseAndCreateOutputFile(Path.Normalize("source", "app.d"), "d.app.app.d")
 	Helpers.ParseAndCreateOutputFile(".gitignore", "d.app.gitignore")
 end
