5b2033a662fa6d0eb89afc02ba9c1d02bed86b60
Paul Crane <paul@acheronsoft.com>
2017-02-04 21:11:14 -0600

    Merge develop

--- generators/d/app/app.lua
+++ generators/d/app/app.lua
@@ -1,7 +1,7 @@
 function OnFinishedInput(values)
 	Path.CreateDirInOutputDir("source")
 
-	Helpers.ParseAndCreateOutputFile("dub.sdl", "dub.sdl")
-	Helpers.ParseAndCreateOutputFile(Path.Normalize("source", "app.d"), "app.d")
-	Helpers.ParseAndCreateOutputFile(".gitignore", "gitignore")
+	Helpers.ParseAndCreateOutputFile("dub.sdl", "d.app.dub.sdl")
+	Helpers.ParseAndCreateOutputFile(Path.Normalize("source", "app.d"), "d.app.app.d")
+	Helpers.ParseAndCreateOutputFile(".gitignore", "d.app.gitignore")
 end
