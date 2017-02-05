2cca251e80e1e09b80754b7483d72a2a500b291b
Paul Crane <paul@acheronsoft.com>
2017-02-04 21:21:39 -0600

    Add additional option to d.lib generator.
    Used for creating a directory inside source directory using the ProjectName.

--- generators/d/lib/lib.lua
+++ generators/d/lib/lib.lua
@@ -1,6 +1,13 @@
 function OnFinishedInput(values)
 	Path.CreateDirInOutputDir("source")
 
+	local createSubDir =
+		UserInput.ConfirmationPrompt("Create additional directory inside source directory using Project Name(y/n?")
+
+	if createSubDir then
+		Path.CreateDirInOutputDir(Path.Normalize("source", UserInput.GetValueFor("ProjectName")))
+	end
+
 	Helpers.ParseAndCreateOutputFile("dub.sdl", "d.lib.dub.sdl")
 	Helpers.ParseAndCreateOutputFile(".gitignore", "d.lib.gitignore")
 end
