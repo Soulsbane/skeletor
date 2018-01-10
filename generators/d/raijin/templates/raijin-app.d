386fc3b28fc68b5b3c07bd5d618c5438754f33e6
Paul Crane <paul@acheronsoft.com>
2018-01-09 23:02:31 -0600

    Update d.raijin generator to latest changes/functionality.

--- generators/d/raijin/templates/raijin-app.d
+++ generators/d/raijin/templates/raijin-app.d
@@ -1,8 +1,20 @@
 import std.stdio;
 
-import raijin;
+import dapplicationbase;
+
+struct Options
+{
+	@GetOptOptions("This is a fake command line argument that is also stored using StructOptions")
+	string fake;
+}
+
+class {{ProjectName}}Application : Application!Options
+{
+	this() {}
+}
 
 void main(string[] arguments)
 {
-	writeln("Raijin imported!!");
+	auto app = new {{ProjectName}}Application;
+	app.create("Raijinsoft", "{{ProjectName}}", arguments);
 }
