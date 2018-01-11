9ee2df681a6660fd1c678c259852be92254a72d7
Paul Crane <paul@acheronsoft.com>
2018-01-11 00:38:20 -0600

    Make waitForApplication API return a value.

--- source/lua/api/process.d
+++ source/lua/api/process.d
@@ -5,10 +5,8 @@ module lua.api.process;
 import std.conv;
 import std.array;
 
-// FIXME: return the value that processwait's function returns.
-// luad gives a compilation issue if return type is auto.
-void waitForApplication(string args)
+int waitForApplication(string args)
 {
 	import processwait;
-	processwait.waitForApplication(args);
+	return processwait.waitForApplication(args);
 }
