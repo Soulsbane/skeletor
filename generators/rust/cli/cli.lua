fcf7a5747d4023d4dd449415c44d5bd593a7c8ba
Paul Crane <paul@acheronsoft.com>
2019-01-25 16:48:25 -0600

    Remove unused functions for rust.cli generator.

--- generators/rust/cli/cli.lua
+++ generators/rust/cli/cli.lua
@@ -1,16 +1,4 @@
-function OnCreate()
-	--This is where initializing global vars etc should go.
-end
-
-function OnProcessInput()
-	--This is where calls to UserInput.Prompt calls should go.
-end
-
 function OnFinishedInput()
 	Helpers.ParseAndCreateOutputFile("Cargo.toml", "rust.cli.Cargo.toml")
 	Helpers.ParseAndCreateOutputFile(Path.Normalize("src", "main.rs"), "rust.cli.main.rs")
-end
-
-function OnDestroy()
-	--Cleaning up any temporary files etc should go here.
 end
