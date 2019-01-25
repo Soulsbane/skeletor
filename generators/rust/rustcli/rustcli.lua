12abe910bef7d087af1b1b46275bd3857cb7f552
Paul Crane <paul@acheronsoft.com>
2019-01-24 22:59:57 -0600

    Add initial files for rust.rustcli generator.

---
+++ generators/rust/rustcli/rustcli.lua
@@ -0,0 +1,15 @@
+function OnCreate()
+	--This is where initializing global vars etc should go.
+end
+
+function OnProcessInput()
+	--This is where calls to UserInput.Prompt calls should go.
+end
+
+function OnFinishedInput()
+	--Calls to IO.CreateOutputFile should go.
+end
+
+function OnDestroy()
+	--Cleaning up any temporary files etc should go here.
+end
